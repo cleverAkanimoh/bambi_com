48135fc5442c2417848cfc699f42cfe9495bae36
nathaniyelll <essien.nathaniyelll@gmail.com>
2024-06-19 00:19:12 +0100

    update OurTeam.tsx

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import teamImg from "../../public/assets/images/team/1.jpg"
 
 const OurTeam = () => {
@@ -60,6 +61,17 @@ const teamData = [
 
                     <h1 className="text-2xl">Our Team</h1>
                     <p className="text-sm mt-2">Accumsan vitae pede lacus ut ullamco</p>
+                </div>
+                <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 md:gap-10 pb-14 md:pb-20">
+                    {
+                     teamData.map(teammember=>(
+                        <div key={teammember.name} className="">
+                            <Image src={teammember.image} alt={teammember.name} />
+                            <h4 className="text-lg">{teammember.name}</h4>
+                            <p className="text-primary">{teammember.title}</p>
+                        </div>
+                     ))   
+                    }
                 </div>
             </div>
         </div>
