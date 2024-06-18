fe5eaa0e68d8999a97e67fa0e7045b475920dba3
nathaniyelll <essien.nathaniyelll@gmail.com>
2024-06-19 00:03:50 +0100

    update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,19 +13,19 @@ const Footer = () => {
     const year = new Date().getFullYear()
 
     return (
-        <footer className="mt-10 bg-secondary pt-20 md:pt-32 text-white">
+        <footer className="mt-10 bg-secondary pt-20 md:pt-24 text-white">
             <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 md:gap-10 pb-14 md:pb-20">
                 <div className="flex flex-col gap-4">
-                    <h1 className="text-xl">About Us</h1>
-                    <p >We create games that inspire growth and development, from
+                    <h1 className="text-lg font-semibold">About Us</h1>
+                    <p  className="text-[14px]">We create games that inspire growth and development, from
                         communication skills and physical health to innovation and creativity, which all play
                         big roles in shaping our children for a positive future.</p>
                     <div className="flex gap-2 items-center">
-                        <a title="Facebook" className="text-primary p-3 hover:text-white hover:bg-[#353535] text-center grid place-items-center rounded-full bg-white" href="#"><FaFacebookF  /></a>
-                        <a title="Twitter" className="text-primary p-3 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><TiSocialTwitter  /></a>
-                        <a title="LinkedIn" className="text-primary p-3 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><FaLinkedinIn  /></a>
-                        <a title="Youtube" className="text-primary p-3 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><TfiYoutube  /></a>
-                        <a title="Vimeo" className="text-primary p-3 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><FaVimeoV  /></a>
+                        <a title="Facebook" className="text-primary p-2 hover:text-white hover:bg-[#353535] text-center grid place-items-center rounded-full bg-white" href="#"><FaFacebookF  /></a>
+                        <a title="Twitter" className="text-primary p-2 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><TiSocialTwitter  /></a>
+                        <a title="LinkedIn" className="text-primary p-2 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><FaLinkedinIn  /></a>
+                        <a title="Youtube" className="text-primary p-2 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><TfiYoutube  /></a>
+                        <a title="Vimeo" className="text-primary p-2 hover:text-white hover:bg-[#151515] text-center grid place-items-center rounded-full bg-white" href="#"><FaVimeoV  /></a>
 
 
                     </div>
@@ -33,7 +33,7 @@ const Footer = () => {
                 </div>
                 <div className="flex flex-col gap-4">
 
-                    <h2 className="text-xl">Contact Us</h2>
+                    <h2 className="text-lg font-semibold">Contact Us</h2>
                     <ul className="flex flex-col gap-3">
                         <li className="flex gap-2 items-center"><IoHomeOutline /><span>147 Akowonjo Road, Lagos</span> </li>
                         <li className="flex gap-2 items-center hover:!text-primary"><MdMailOutline /><a href="mailto:info@example.com">
@@ -43,12 +43,12 @@ const Footer = () => {
 
                 </div>
                 <div className="flex flex-col gap-4">
-                    <h2 className="text-xl">Information</h2>
-                    <a href="#">Terms and Conditions</a>
+                    <h2 className="text-lg font-semibold">Information</h2>
+                    <a className="text-[14px]" href="#">Terms and Conditions</a>
 
                 </div>
                 <form className="flex flex-col gap-4">
-                    <h2 className="text-xl">Sign Up for Newsletter</h2>
+                    <h2 className="text-lg font-semibold">Sign Up for Newsletter</h2>
                     <div className="flex flex-col gap-2">
                         <input className="w-full p-2 outline-none ring-0 bg-slate-100 focus:bg-white border focus:border-primary" type="email" placeholder="info@teenscancode.com.ng" name="email" />
                         <button
@@ -59,7 +59,7 @@ const Footer = () => {
                             Subscribe
                         </button>
                     </div>
-                    <p>Join over 1,000 people who get free and fresh content
+                    <p className="text-[14px]">Join over 1,000 people who get free and fresh content
                         delivered automatically each time we publish</p>
 
                 </form>
