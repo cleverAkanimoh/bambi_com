0e4564c3b4465a66167956a664eb52319844c4ca
nathaniyelll <essien.nathaniyelll@gmail.com>
2024-06-19 01:25:28 +0100

    update page.tsx

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,9 +1,37 @@
+"use client"
+import Accordion from '@/components/Accordion';
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>Frequently Asked Questions Page</div>
-  )
+    const accordionItems = [
+        {
+            title: "FAQ 1",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis molestias nulla praesentium magni. Ea repellendus facere quod minima amet porro, voluptatum qui labore veniam maxime aspernatur animi sit dolor velit et! Asperiores quis, ullam unde fuga rem"
+        },
+        {
+            title: "FAQ 2",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis molestias nulla praesentium magni. Ea repellendus facere quod minima amet porro, voluptatum qui labore veniam maxime aspernatur animi sit dolor velit et! Asperiores quis, ullam unde fuga rem"
+        },
+        {
+            title: "FAQ 3",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis molestias nulla praesentium magni. Ea repellendus facere quod minima amet porro, voluptatum qui labore veniam maxime aspernatur animi sit dolor velit et! Asperiores quis, ullam unde fuga rem"
+        }
+    ];
+
+
+    return (
+        <div className='my-8'>
+            <div className='w-[90%] mx-auto grid gap-10'>
+                <div className='grid gap-2'>
+                    <h1 className='text-xl font-bold'>Below are frequently asked questions, you may find the answer for yourself</h1>
+                    <p className='text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis molestias nulla praesentium magni. Ea repellendus facere quod minima amet porro, voluptatum qui labore veniam maxime aspernatur animi sit dolor velit et! Asperiores quis, ullam unde fuga rem </p>
+                </div>
+                <div className="">
+                    <Accordion items={accordionItems} />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default page
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { useState } from 'react';
+
+interface AccordionItemProps {
+    title: string;
+    content: string;
+    isOpen: boolean;
+    onToggle: () => void;
+}
+
+interface AccordionProps {
+    items: {
+        title: string;
+        content: string;
+    }[];
+}
+
+const AccordionItem: React.FC<AccordionItemProps> = ({ title, content, isOpen, onToggle }) => (
+  <div className={`border-b border-b-primary`}>
+    <button
+      className={`w-full text-left py-4 px-4 focus:outline-none flex justify-between items-center ${isOpen && "bg-primary text-white"}`}
+      onClick={onToggle}
+    >
+      <span className='font-bold'>{title}</span>
+      <span className='font-bold'>{isOpen ? '-' : '+'}</span>
+    </button>
+    {isOpen && <div className="p-6 text-sm">{content}</div>}
+  </div>
+);
+
+const Accordion: React.FC<AccordionProps> = ({ items }) => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const handleToggle = (index: number) => {
+    setOpenIndex(index === openIndex ? null : index);
+  };
+
+  return (
+    <div className="w-full">
+      {items.map((item, index) => (
+        <AccordionItem
+          key={index}
+          title={item.title}
+          content={item.content}
+          isOpen={openIndex === index}
+          onToggle={() => handleToggle(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
