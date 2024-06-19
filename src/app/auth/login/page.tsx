d9b6bd200c2439d0e88550782d8d4b5cac3df081
nathaniyelll <essien.nathaniyelll@gmail.com>
2024-06-19 18:40:43 +0100

    update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -75,8 +75,7 @@ const Page = () => {
                 <button
 
                     type="submit"
-                    className='self-start text-white w-full md:w-[40%] lg:w-[30%] text-center font-bold bg-black p-4 hover:bg-primary hover:text-white transition-all ease-in-out duration-200'
-
+                    className={`self-start text-white w-full md:w-[40%] lg:w-[30%] text-center font-bold bg-black p-4 hover:bg-primary hover:text-white transition-all ease-in-out duration-200 ${isSubmitted ? "opacity-70" : "opacity-100"}`}
                     disabled={isSubmitted}
                 >
                     {isSubmitted ? "Logging in..." : "Login"}
