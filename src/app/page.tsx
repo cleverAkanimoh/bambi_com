c59e8beb0fc0aff7370024da473a046ad1959300
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-19 12:21:28 +0100

    modified page.tsx added Brands.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import BannerSection from "@/components/home/BannerSection";
+import Brands from "@/components/home/Brands";
 import CtaSection from "@/components/home/CtaSection";
 import Header from "@/components/home/Header";
 import Products from "@/components/home/Products";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="overflow-x-hidden">
       <Header />
       <Products />
       {/* <BannerSection /> */}
       <CtaSection />
+      <Brands />
     </main>
   );
 }
