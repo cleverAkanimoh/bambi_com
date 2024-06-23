93f0ac1b48588c428c9a5cfa63d005a39b1d5f36
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-23 01:42:21 +0100

    modified CartOffCanvas.tsx in navbar

--- src/components/navbar/CartOffCanvas.tsx
+++ src/components/navbar/CartOffCanvas.tsx
@@ -12,6 +12,8 @@ import { useAuth } from "@/context/auth-context";
 import { cartItemRef } from "@/lib/cart";
 import clsx from "clsx";
 import { useGlobalContext } from "@/context/store";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import BamIcon from "../Icon";
 
 export const fetchInRealtimeAndRenderPostsFromDB = async () => {
   const snapshot = await getDocs(cartItemRef);
@@ -62,86 +64,96 @@ export default function CartOffCanvas() {
         onClick={() => setIsCartClicked(false)}
       />
 
-      {/* <!-- Cart Offcanvas Inner Start --> */}
-      <aside className="">
-        {/* <!-- Button Close Start --> */}
-        <div className="offcanvas-btn-close">
-          <i className="pe-7s-close"></i>
+      <div
+        className={clsx("relative w-full flex transition-all duration-500", {
+          "translate-x-0 opacity-100 visible": isCartClicked,
+          "translate-x-full opacity-0 invisible": !isCartClicked,
+        })}
+      >
+        <div onClick={() => setIsCartClicked(false)}>
+          <button className="bg-primary size-10 text-white grid place-items-center">
+            <BamIcon
+              Icon={XMarkIcon}
+              size="big"
+              className="hover:rotate-90 transition-all duration-500"
+            />
+          </button>
         </div>
-        {/* <!-- Button Close End --> */}
+        <aside
+          className={clsx(
+            "w-11/12 xs:min-w-[320px] shrink-0 relative h-screen overflow-y-auto p-2 xs:p-4 py-10 bg-white"
+          )}
+        >
+          {user ? (
+            <>
+              {cartItems.length > 0 ? (
+                <div className="offcanvas-cart-content">
+                  <div className="w-full flex items-center justify-between mb-3 -mt-8">
+                    <Link href="/shop" className="hover:underline text-primary">
+                      Continue shopping
+                    </Link>
+                    <DeleteAllCartItemsButton />
+                  </div>
+                  {cartItems.map((item, index) => (
+                    <CartTile
+                      key={index}
+                      src={item.src}
+                      href={item.href}
+                      title={item.title}
+                      price={item.price}
+                      quantity={item.quantity}
+                      id={item.id}
+                    />
+                  ))}
 
-        {/* check if user is logged in */}
+                  {/* <!-- Cart Product Total Start --> */}
+                  <div className="cart-product-total mb-4 pb-4 border-bottom">
+                    <span className="value">Total</span>
+                    <span className="price">${cartTotal}</span>
+                  </div>
+                  {/* <!-- Cart Product Total End --> */}
 
-        {user ? (
-          <>
-            {cartItems.length > 0 ? (
-              <div className="offcanvas-cart-content">
-                <div className="w-full flex items-center justify-between mb-3 -mt-8">
-                  <Link href="/shop" className="hover:underline text-primary">
-                    Continue shopping
-                  </Link>
-                  <DeleteAllCartItemsButton />
+                  {/* <!-- Cart Product Button Start --> */}
+                  <div className="cart-product-btn mt-4">
+                    <Link
+                      href="/cart"
+                      className="btn btn-light btn-hover-primary w-100"
+                    >
+                      <i className="fa fa-shopping-cart"></i> View cart
+                    </Link>
+                    <Link
+                      href="/checkout"
+                      className="btn btn-light btn-hover-primary w-100 mt-4"
+                    >
+                      <i className="fa fa-share"></i> Checkout
+                    </Link>
+                  </div>
+                  {/* <!-- Cart Product Button End --> */}
                 </div>
-                {cartItems.map((item, index) => (
-                  <CartTile
-                    key={index}
-                    src={item.src}
-                    href={item.href}
-                    title={item.title}
-                    price={item.price}
-                    quantity={item.quantity}
-                    id={item.id}
-                  />
-                ))}
-
-                {/* <!-- Cart Product Total Start --> */}
-                <div className="cart-product-total mb-4 pb-4 border-bottom">
-                  <span className="value">Total</span>
-                  <span className="price">${cartTotal}</span>
-                </div>
-                {/* <!-- Cart Product Total End --> */}
-
-                {/* <!-- Cart Product Button Start --> */}
-                <div className="cart-product-btn mt-4">
-                  <Link
-                    href="/cart"
-                    className="btn btn-light btn-hover-primary w-100"
-                  >
-                    <i className="fa fa-shopping-cart"></i> View cart
+              ) : (
+                <section className="h-full flex flex-col gap-5 items-center justify-center">
+                  <CiShoppingCart size={130} className="opacity-60" />
+                  <h3>Nothing in cart</h3>
+                  <Link href="/shop" className="hover:text-primary">
+                    Go to shop
                   </Link>
-                  <Link
-                    href="/checkout"
-                    className="btn btn-light btn-hover-primary w-100 mt-4"
-                  >
-                    <i className="fa fa-share"></i> Checkout
-                  </Link>
-                </div>
-                {/* <!-- Cart Product Button End --> */}
-              </div>
-            ) : (
-              <section className="h-full flex flex-col gap-5 items-center justify-center">
-                <CiShoppingCart size={130} className="opacity-60" />
-                <h3>Nothing in cart</h3>
-                <Link href="/shop" className="hover:text-primary">
-                  Go to shop
-                </Link>
-              </section>
-            )}
-          </>
-        ) : (
-          <section className="h-full flex flex-col gap-5 items-center justify-center">
-            <h4>No user Found</h4>
-            <p>You have to login to see your cart item</p>
-            <Link
-              href="/auth/login"
-              className="bg-primary p-2 text-white hover:opacity-60"
-            >
-              Take me to login
-            </Link>
-          </section>
-        )}
-      </aside>
-      {/* <!-- Cart Offcanvas Inner End --> */}
+                </section>
+              )}
+            </>
+          ) : (
+            <section className="h-full flex flex-col gap-5 items-center justify-center">
+              <h4>No user Found</h4>
+              <p>You have to login to see your cart item</p>
+              <Link
+                href="/auth/login"
+                className="bg-primary p-2 text-white hover:opacity-60"
+              >
+                Take me to login
+              </Link>
+            </section>
+          )}
+        </aside>
+      </div>
     </section>
   );
 }
