1f8e3d57d085871dc4a0b9aaac97dab34b7af1c0
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-24 21:19:43 +0100

    modified page.tsx and AdditionalInformationForProduct.tsx

--- src/app/shop/[productId]/page.tsx
+++ src/app/shop/[productId]/page.tsx
@@ -22,7 +22,7 @@ export default function SingleProductPage({
   return (
     <main className="flex flex-col">
       <Breadcrumbs active="Single Product Page" />
-      <section className="my-14">
+      <section className="my-8">
         {/* dynamic */}
         <Suspense>
           <ProductDisplay singleProduct={singleProduct} />
