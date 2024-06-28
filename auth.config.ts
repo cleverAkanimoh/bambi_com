08f713e236be4ac9d1e1801e8ba777eb1e62358d
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-28 12:53:04 +0100

    modified auth.config.ts in .

--- auth.config.ts
+++ auth.config.ts
@@ -18,6 +18,8 @@ export const authConfig = {
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/", nextUrl));
       }
 
       return true;
