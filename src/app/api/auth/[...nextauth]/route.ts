9445a184951b76353159a9c4ef3eeb2c4b12e7aa
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-29 10:37:50 +0100

    modified route.ts in [...nextauth]

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,5 @@
-import { NextResponse, NextRequest } from "next/server";
-import { loginUserAction } from "@/actions/authenticate";
-import CredentialsProvider from "next-auth/providers/credentials";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import { authOptions } from "../../../../../auth";
-
-
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
